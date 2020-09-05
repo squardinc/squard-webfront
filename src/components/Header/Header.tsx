8fe03c08adfb5856d5534578f5fe1e2a6b69aca3
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-05 12:38:33 +0900

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './Header.module.scss'
 
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <FontAwesomeIcon icon={faBars} />
+        Squard
+        <FontAwesomeIcon icon={faSearch} />
+      </div>
+    </div>
+  )
 }
 
