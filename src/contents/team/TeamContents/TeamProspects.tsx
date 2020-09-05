399d9c0fca31473d15551fe0618c0f43987d0ded
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-05 23:29:53 +0900

    refactor prospects

--- src/contents/team/TeamContents/TeamProspects.tsx
+++ src/contents/team/TeamContents/TeamProspects.tsx
@@ -2,6 +2,17 @@ import * as React from 'react'
 import { LeftBorderCaption } from 'src/components/Caption/Captions'
 import styles from './TeamProspects.module.scss'
 
+interface ProspectProps {
+  prospect: string
+}
+const Prospect: React.FC<ProspectProps> = ({ prospect }) => {
+  return (
+    <div className={styles.prospectContainer} >
+      <img src={prospect} className={styles.prospect} />
+    </div>
+  )
+}
+
 interface TeamPropspectsProps {
   propspects: string[]
 }
@@ -9,9 +20,9 @@ export const TeamProspects: React.FC<TeamPropspectsProps> = ({ propspects }) => 
   return (
     <div className={styles.container}>
       <LeftBorderCaption text='PROSPECTS' color='white' />
-      <div className={styles.members}>
-        {propspects.map(member =>
-          <img src={member} className={styles.member} />
+      <div className={styles.prospects}>
+        {propspects.map(prospect =>
+          <Prospect prospect={prospect} />
         )}
       </div>
     </div >
