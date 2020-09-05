d7fa72744b884eff1e56f8f31b8a4b96126ad51c
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-05 19:33:18 +0900

    extract dummy data to parent

--- src/contents/team/TeamAngels.tsx
+++ src/contents/team/TeamAngels.tsx
@@ -4,20 +4,16 @@ import { TwoStagedCaption } from 'src/components/Caption/Captions'
 
 import styles from './TeamAngels.module.scss'
 
-const CORE_MEMBERS = [
-  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fshunpei.png?alt=media&token=3e8b258d-fb7d-4437-ac86-298573471d81',
-  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fmatsui.png?alt=media&token=6b84927f-4069-40ac-8a90-09099a7f71ab',
-  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fkimura.png?alt=media&token=ce7a24bd-b92d-4c31-a803-90263e76d52a',
-  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fshoya.png?alt=media&token=50e1a69b-2f2c-4998-bba9-8fc3deb7d06d',
-]
-
-const numOfAngels = 65535
-export const TeamAngels = () => {
+interface TeamAngelsProps {
+  angels: string[]
+  numOfAngels: number
+}
+export const TeamAngels: React.FC<TeamAngelsProps> = ({ angels, numOfAngels }) => {
   return (
     <div className={styles.container}>
       <TwoStagedCaption sub='And we have' main={`${numOfAngels} Angels`} style='medium' />
       <div className={styles.members}>
-        {CORE_MEMBERS.map(member =>
+        {angels.map(member =>
           <img src={member} className={styles.member} />
         )}
       </div>
--- src/contents/team/TeamLayout.tsx
+++ src/contents/team/TeamLayout.tsx
@@ -7,16 +7,23 @@ import { TeamProspects } from './TeamProspects'
 import { TeamAngels } from './TeamAngels'
 import { TeamVIP } from './TeamVIP'
 
+const DUMMY_MEMBERS = [
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fshunpei.png?alt=media&token=3e8b258d-fb7d-4437-ac86-298573471d81',
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fmatsui.png?alt=media&token=6b84927f-4069-40ac-8a90-09099a7f71ab',
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fkimura.png?alt=media&token=ce7a24bd-b92d-4c31-a803-90263e76d52a',
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fshoya.png?alt=media&token=50e1a69b-2f2c-4998-bba9-8fc3deb7d06d',
+]
+
 export const TeamLayout = () => {
   return (
     <>
       <TeamTop />
       <TeamIntroduction />
-      <TeamCoreMembers />
-      <TeamMembers />
-      <TeamProspects />
-      <TeamAngels />
-      <TeamVIP />
+      <TeamCoreMembers coreMembers={DUMMY_MEMBERS} />
+      <TeamMembers members={DUMMY_MEMBERS} />
+      <TeamProspects propspects={DUMMY_MEMBERS} />
+      <TeamAngels angels={DUMMY_MEMBERS} numOfAngels={65535} />
+      <TeamVIP vips={DUMMY_MEMBERS} />
     </>
   )
 }
