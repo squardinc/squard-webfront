55549fd583f86fe5a3c7d6402359b8641e0f94a0
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-05 14:59:49 +0900

    add core members section

---
+++ src/contents/team/TeamCoreMembers.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { StaticImageFluid, StaticImageFixed } from 'src/components/Image'
+import styles from './TeamCoreMembers.module.scss'
+
+const CORE_MEMBERS = [
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fshunpei.png?alt=media&token=3e8b258d-fb7d-4437-ac86-298573471d81',
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fmatsui.png?alt=media&token=6b84927f-4069-40ac-8a90-09099a7f71ab',
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fkimura.png?alt=media&token=ce7a24bd-b92d-4c31-a803-90263e76d52a',
+  'https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/squard%2Fshoya.png?alt=media&token=50e1a69b-2f2c-4998-bba9-8fc3deb7d06d',
+]
+
+export const TeamCoreMembers = () => {
+  return (
+    <div className={styles.container}>
+      <StaticImageFluid image='headlineCoremebers' />
+      <div className={styles.members}>
+        {CORE_MEMBERS.map(member =>
+          //  <StaticImageFixed image={member} />
+          <img src={member} className={styles.member} />
+        )}
+      </div>
+    </div >
+  )
+}
--- src/contents/team/TeamLayout.tsx
+++ src/contents/team/TeamLayout.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { TeamTop } from './TeamTop'
 import { TeamIntroduction } from './TeamIntroduction'
+import { TeamCoreMembers } from './TeamCoreMembers'
 
 export const TeamLayout = () => {
   return (
     <>
       <TeamTop />
       <TeamIntroduction />
+      <TeamCoreMembers />
     </>
   )
 }
