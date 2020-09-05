1163c9656ae293d0f403064d88b6f2bd367760a7
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-05 15:01:04 +0900

    add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,42 @@
 import * as React from 'react'
+import { StaticImageFluid } from '../Image'
+
+import styles from './Footer.module.scss'
+import { DefaultButton } from '../Button/DefaultButton'
+import { Link } from 'gatsby'
 
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.background}>
+        <StaticImageFluid image='background' />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.titleSub}>What's the</div>
+        <div className={styles.titleMain}>Squard?</div>
+        <div className={styles.description}>
+          Webサービス「Squard（スクアード）」は、個人でもなく法人でもない新しい働き方の実現を目標に、新時代のチームメイキングソリューションを提供するコラボレーションプラットフォームです。
+          <br />
+          チームページからProspects（プロスペクト）またはAngels（エンジェル）としてチームに参加することで、チームが定めた各種特典を受け取ることができます。
+        </div>
+        <div className={styles.question}>
+          「Prospects」や「Angels」についてはこちら↓
+        </div>
+        <DefaultButton text='Class（クラス）ってなに？' />
+        <div className={styles.sitemap}>
+          <div className={styles.links}>
+            <Link to='/'>FAQ</Link>
+            <Link to='/'>About</Link>
+            <Link to='/'>Company</Link>
+            <Link to='/'>Privacy Policy</Link>
+          </div>
+          <StaticImageFluid image='footerLogo' />
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        Copyright ©2020 Squard, Inc. All Rights Reseverd.
+      </div>
+    </div>
+  )
 }
 
