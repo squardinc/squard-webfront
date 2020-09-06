82510e870fa9e23bbbeaa42b06ec6bd5da6413e5
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-07 00:08:09 +0900

    enable to load env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
+const dotenv = require('dotenv')
 const path = require('path')
+dotenv.config({ path: `env/.${process.env.BRANCH === 'master' ? 'master' : 'master'}` })
 
 module.exports = {
   siteMetadata: {
