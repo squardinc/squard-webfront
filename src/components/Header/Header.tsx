03aad88e67aecde4bc1375ba9f100e9028186ed1
tanvirjewel <tanvirjewel@gmail.com>
2020-09-17 05:28:42 +0600

    search bar added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,12 +26,18 @@ export const Header = () => {
           <FontAwesomeIcon icon={faSearch} size="2x" cursor="pointer" onClick={toggleSearch} />
         </div>
       </div>
-      <div id="search" className="pt-2 relative mx-auto text-gray-600" style={{ display: "none" }}>
-        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-          type="search" name="search" placeholder="Search" />
-        <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
-          <FontAwesomeIcon icon={faSearch} size="2x" />
-        </button>
+      <div className={styles.searchBar}>
+        <div id="search" className="pt-2 pl-8 relative mx-auto text-gray-600 w-full" style={{ display: "none" }}>
+          <input className="border-2 border-gray-300 bg-white h-12 w-full px-5 pr-16 rounded-full text-sm focus:outline-none"
+            type="search" name="search" placeholder="Search" />
+          <div className="absolute right-0 top-0 mt-2 ml-4">
+          <div className={styles.searchBtn}>
+            <button type="submit" >
+              <FontAwesomeIcon icon={faSearch} size="lg" color="black" style={{marginTop: "15px"}} className="ml-4"/>
+            </button>
+          </div>
+        </div>
+        </div>
       </div>
     </React.Fragment>
   )
