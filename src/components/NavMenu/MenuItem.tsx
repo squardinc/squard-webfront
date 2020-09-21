11286d03450dc80ef46f529fc1f1a4aa0aa5ccd4
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-21 23:54:20 +0900

    enable to open login modal

--- src/components/NavMenu/MenuItem.tsx
+++ src/components/NavMenu/MenuItem.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import styles from './NavMenu.module.scss'
 
-export interface MenuItemProps {
+interface MenuItemProps {
   text: string
   SVGIcon: any
   className?: string
+  onClick?: VoidFunction
 }
-export const MenuItem: React.FC<MenuItemProps> = ({ text, SVGIcon, className='' }) => {
+export const MenuItem: React.FC<MenuItemProps> = ({ text, SVGIcon, className = '', onClick = () => { } }) => {
   return (
-    <div className={`flex w-full ${className}`}>
+    <div className={`flex w-full cursor-pointer ${className}`} onClick={onClick}>
       <SVGIcon className="h-16 w-auto" />
       <div className={"mt-4 flex-grow w-auto font-thin text-lg border-b border-gray-100 border-opacity-75 " + styles.navText}>{text}</div>
     </div>
