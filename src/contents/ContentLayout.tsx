a418a23c143ec2c291051d3e436a6b5e41f45b56
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-23 07:47:22 +0900

    add developing pages

--- src/contents/ContentLayout.tsx
+++ src/contents/ContentLayout.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react'
+import { AboutPage } from './about'
+import { CompanyPage } from './company'
+import { FAQPage } from './faq'
+import { PrivacyPolicyPage } from './privacypolicy'
 import { TeamLayout } from './team/TeamLayout'
+import { PersonLayout } from './person/PersonLayout'
 
 interface ContentLayoutProps {
   path: string
   contentId: string
 }
 export const ContentLayout: React.FC<ContentLayoutProps> = ({ contentId }) => {
-  return <TeamLayout />
+  switch (contentId) {
+    case 'about':
+      return <AboutPage />
+    case 'company':
+      return <CompanyPage />
+    case 'faq':
+      return <FAQPage />
+    case 'privacypolicy':
+      return <PrivacyPolicyPage />
+  }
+  if (contentId === 'squard') {
+    return <TeamLayout />
+  }
+  return <PersonLayout />
 }
