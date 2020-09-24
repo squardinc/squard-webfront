0f573f356a0172e0537704f09150e55f5d01f27d
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-25 03:43:48 +0900

    add redirection on accessed unexpected path

--- src/contents/top.tsx
+++ src/contents/top.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { TeamLayout } from './team/TeamLayout'
 
 interface TopLayoutProps {
-  default: boolean
+  path: string
 }
 export const TopLayout: React.FC<TopLayoutProps> = () => {
   return <TeamLayout />
