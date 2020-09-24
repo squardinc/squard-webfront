c7a64cd4e1451edaa26a563aaedc4f6327a003a7
Ryosuke Miki <ryosuke.miki@outlook.jp>
2020-09-24 12:08:04 +0900

    adjust style

--- src/components/Rocket/Rocket.tsx
+++ src/components/Rocket/Rocket.tsx
@@ -6,12 +6,6 @@ import RocketBack from 'src/assets/about/rocket_back.svg'
 import RocketFront from 'src/assets/about/rocket_front.svg'
 
 export const Rocket: React.FC = (props) => {
-  const wrapper = React.useRef();
-
-  React.useEffect(() => {
-    console.log(wrapper.current);
-  });
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.rocketBack}>
