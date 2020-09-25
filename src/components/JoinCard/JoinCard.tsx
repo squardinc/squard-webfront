cab58e8d9742485f29985b2dffaecc67c3552fb2
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-26 04:45:19 +0900

    open logon modal if not logged in on click join

--- src/components/JoinCard/JoinCard.tsx
+++ src/components/JoinCard/JoinCard.tsx
@@ -4,37 +4,45 @@ import styles from './JoinCard.module.scss'
 
 import { addComma } from 'src/utils/NumberFormatter'
 import { TextDisplay } from '../TextDisplay/TextDisplay'
+import { RoundButton } from '../Button/DefaultButton'
 
 interface JoinCardProps {
-  main: string
+  roleId: string
   sub: string
   monthlyPrice: number
   entitlements: string[]
+  loggedIn: boolean
+  login: VoidFunction
+  join: (roleId: string) => Promise<void>
 }
 
-export const JoinCard: React.FC<JoinCardProps> = (props) => {
-  const formattedPrice = addComma(props.monthlyPrice)
+export const JoinCard: React.FC<JoinCardProps> = ({ roleId, sub, monthlyPrice, entitlements, loggedIn, login, join }) => {
+  const formattedPrice = addComma(monthlyPrice)
 
   return (
     <div className={styles.card}>
       <div className={styles.flagWrapper}>
         <div className={styles.flag}>
-          <TextDisplay className={styles.nameMain}>{props.main}</TextDisplay>
-          <TextDisplay className={styles.nameSub}>（{props.sub}）</TextDisplay>
+          <TextDisplay className={styles.nameMain}>{roleId}</TextDisplay>
+          <TextDisplay className={styles.nameSub}>（{sub}）</TextDisplay>
           <TextDisplay className={styles.price}>
             ￥{formattedPrice} / 月額
           </TextDisplay>
         </div>
       </div>
       <div className={styles.entitlements}>
-        {props.entitlements.map((el, index) => (
+        {entitlements.map((el, index) => (
           <TextDisplay key={index} className={styles.entitlement}>
             {el}
           </TextDisplay>
         ))}
       </div>
       <div className={styles.joinNowWrapper}>
-        <TextDisplay className={styles.joinNow}>今すぐ参加する</TextDisplay>
+        <RoundButton
+          className={styles.joinNow}
+          text='今すぐ参加する'
+          onClick={() => loggedIn ? join(roleId) : login()}
+        />
       </div>
     </div>
   )
