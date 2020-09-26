5f869c043e3e1ee9ba144ffe15531df7a4005f11
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-26 22:30:53 +0900

    fix external link

--- src/contents/about/AboutPage.tsx
+++ src/contents/about/AboutPage.tsx
@@ -6,12 +6,9 @@ import { Classes } from './Classes/Classes'
 import { ManagementSystem } from './ManagementSystem/ManagementSystem'
 import { Status } from './Status/Status'
 import { ContentFooter } from 'src/components/Footer/ContentFooter'
-import { LoginModal } from 'src/components/Modal/LoginModal'
 import { withTheme } from 'src/context/ThemeContext'
 
 const Page: React.FC = () => {
-  const [showLoginModal, setShowLoginModal] = React.useState(false)
-  const [showSignUpModal, setShowSignUpModal] = React.useState(false)
   return (
     <div>
       <Heading1>About</Heading1>
@@ -19,19 +16,17 @@ const Page: React.FC = () => {
       <Classes></Classes>
       <Status></Status>
       <ManagementSystem></ManagementSystem>
-      <ContentFooter
-        titleSub='Thank you for'
-        titleMain='Readiong.'
-        text='こちらのページを最後まで読んでくださったことを心より感謝いたします。Squardは現在ベータ版でのご提供となりますが2020年11月の正式版リリースに伴い皆様にも自由にチームを作成していただくことが可能となります。以下より事前登録を行っていただくことで正式版ローンチのご連絡をさせていただきます。'
-        buttonText='事前登録フォームに進む'
-        onButtonClick={() => setShowLoginModal(true)}
-      />
-      <div>{showLoginModal ? <LoginModal closeModal={() => setShowLoginModal(false)} showSignUpModal={() => {
-        setShowLoginModal(false)
-        setShowSignUpModal(true)
-      }} /> : <></>}</div>
+      <a href='https://www.squard.co.jp/coming-soon/'>
+        <ContentFooter
+          titleSub='Thank you for'
+          titleMain='Readiong.'
+          text='こちらのページを最後まで読んでくださったことを心より感謝いたします。Squardは現在ベータ版でのご提供となりますが2020年11月の正式版リリースに伴い皆様にも自由にチームを作成していただくことが可能となります。以下より事前登録を行っていただくことで正式版ローンチのご連絡をさせていただきます。'
+          buttonText='事前登録フォームに進む'
+          onButtonClick={() => { }}
+        />
+      </a>
     </div>
   )
 }
 
-export const AboutPage =  withTheme(Page, 'dark')
+export const AboutPage = withTheme(Page, 'dark')
