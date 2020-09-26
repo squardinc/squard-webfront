44f3571641c58e0954ce58e4a2e5f5aac4c91dfb
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-26 22:00:02 +0900

    remove unused

--- src/contents/SignUp.tsx
+++ src/contents/SignUp.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import { TopLayout } from './top'
 import { SignUpLoginLayout } from 'src/components/Modal/SignUpLoginModal'
+import { TeamLayout } from './team/TeamLayout'
 
 export const SignUpLayout: React.FC = () => {
   const [showSignUpModal, setShowSignUpModal] = React.useState(true)
   const [showLoginModal, setShowLoginModal] = React.useState(false)
   return (
     <>
-      <TopLayout />
+      <TeamLayout />
       <SignUpLoginLayout
         showSignUpModal={showSignUpModal}
         showLoginModal={showLoginModal}
