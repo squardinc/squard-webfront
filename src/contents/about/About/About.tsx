1204e19b42afaf8486552f192a62fd68755dd564
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-26 23:35:13 +0900

    fix about page

--- src/contents/about/About/About.tsx
+++ src/contents/about/About/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC<AboutProps> = (props) => {
   return (
     <div>
       <Description>
-        Squard（スクワード）は個人でも法人でもない新しいチームの形を定義するコラボレーションプラットフォームです。
+        Squard（スクアード）は個人でも法人でもない新しいチームの形を定義するコラボレーションプラットフォームです。
         Class（クラス）によって参加者や後援者の区分を最適化することで、プロジェクトの成功に向けて最前線を走る人もそれを応援する人も、それぞれが最も活躍できる環境を実現しています。
       </Description>
       <div className={styles.visualizeArea}>
