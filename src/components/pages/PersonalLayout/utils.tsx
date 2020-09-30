c8c3332f5a9019dc003f30a5fccda7a5d171f879
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-09-30 20:35:58 +0900

    reorg assets dir

--- src/components/pages/PersonalLayout/utils.tsx
+++ src/components/pages/PersonalLayout/utils.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react'
-import Facebook from 'src/assets/facebook_icon.svg'
-import Instagram from 'src/assets/instagram_icon.svg'
-import Twitter from 'src/assets/twitter_icon.svg'
-import Youtube from 'src/assets/youtube_icon.svg'
-import Email from 'src/assets/email_icon.svg'
-import Phone from 'src/assets/phone_icon.svg'
-import Link from 'src/assets/link_icon.svg'
-import Zoom from 'src/assets/zoom_icon.svg'
+import Facebook from 'src/assets/social_media/facebook.svg'
+import Instagram from 'src/assets/social_media/instagram.svg'
+import Twitter from 'src/assets/social_media/twitter.svg'
+import Youtube from 'src/assets/social_media/youtube.svg'
+import Email from 'src/assets/social_media/email.svg'
+import Phone from 'src/assets/social_media/phone.svg'
+import Link from 'src/assets/social_media/link.svg'
+import Zoom from 'src/assets/social_media/zoom.svg'
 
-import SquardLeader from 'src/assets/squard_leader.svg'
-import CoreMembers from 'src/assets/core_members.svg'
+import Leader from 'src/assets/classes/leader.svg'
+import CoreMembers from 'src/assets/classes/core_members.svg'
+import Members from 'src/assets/classes/members.svg'
+import Prospects from 'src/assets/classes/prospects.svg'
+import Angels from 'src/assets/classes/angels.svg'
+import Galleries from 'src/assets/classes/galleries.svg'
+import VIP from 'src/assets/classes/vip.svg'
 
-import { SocialMediaType, TeamType } from 'src/models/personal'
+import { SocialMediaType, ClassType } from 'src/models/personal'
 
-export function getSocialMediaIcon(name: SocialMediaType) {
+export const getSocialMediaIcon = (name: SocialMediaType) => {
   if (name === 'email') {
     return <Email />
   } else if (name === 'facebook') {
@@ -28,15 +33,29 @@ export function getSocialMediaIcon(name: SocialMediaType) {
     return <Phone />
   } else if (name === 'zoom') {
     return <Zoom />
-  } else if (name === 'link') {
-    return <Link />
   }
+  return <Link />
 }
 
-export function getTeamIcon(name: TeamType) {
-  if (name === 'squard') {
-    return <SquardLeader width="3rem" height="3rem" />
-  } else if (name === 'cheerfully') {
-    return <CoreMembers width="3rem" height="3rem" />
+const teamIconProps = {
+  width: '3rem',
+  height: '3rem'
+}
+export const getTeamIcon = (classType: ClassType) => {
+  switch (classType) {
+    case 'Leader':
+      return <Leader {...teamIconProps} />
+    case 'Core Members':
+      return <CoreMembers {...teamIconProps} />
+    case 'Members':
+      return <Members {...teamIconProps} />
+    case 'Prospects':
+      return <Prospects {...teamIconProps} />
+    case 'Angels':
+      return <Angels {...teamIconProps} />
+    case 'VIP':
+      return <VIP {...teamIconProps} />
+    default:
+      return <Galleries {...teamIconProps} />
   }
 }
