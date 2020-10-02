0b3a53e26cce8ea883fa9c1cc2b70cc5d30d9113
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-03 02:30:04 +0900

    add validator

---
+++ src/utils/StringValidator.ts
@@ -0,0 +1,5 @@
+const EMAIL_ADDRESS_CHAR_REGEX = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~\-@]*$/
+const EMAIL_ADDRESS_REGEX = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+export const emaliAvaliableChars = (str: string) => EMAIL_ADDRESS_CHAR_REGEX.test(str)
+export const validEmaliAddress = (str: string) => EMAIL_ADDRESS_REGEX.test(str)
