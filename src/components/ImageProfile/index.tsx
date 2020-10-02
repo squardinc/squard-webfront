888f1ea0e30494600c808fe641e9b885d1817567
Dinh Quoc Hung <dqhung215@gmail.com>
2020-10-02 19:20:15 +0900

    update ui

--- src/components/ImageProfile/index.tsx
+++ src/components/ImageProfile/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { Image } from '../Image/index'
+import { Icon } from '../Icon'
+import Camera from '../../assets/icon-camera.svg'
 export interface ImageProfileProps {
   cover?: string
   avatar?: string
@@ -27,12 +29,40 @@ const ImageAvatarWrapper = styled.div`
   height: 150px;
 `
 
+const CameraIconCoverWrapper = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 150px;
+  height: 150px;
+  display:flex;
+  justify-content: center;
+  align-items:center;
+`
+
+const CameraIconAvatarWrapper = styled.div`
+  position: absolute;
+  top: 175px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 150px;
+  height: 150px;
+  display:flex;
+  justify-content: center;
+  align-items:center;
+`
+
 export const ImageProfile = React.memo((props: ImageProfileProps) => {
   const style = props.style ? { ...props.style } : {}
 
-  const cover = props.cover
-  const avatar = props.avatar
-  
+  const cover = props.cover ? props.cover : ''
+  const avatar = props.avatar ? props.avatar : ''
+
   return (
     <ImageProfileWrapper style={style}>
       <ImageCoverWrapper>
@@ -53,6 +83,13 @@ export const ImageProfile = React.memo((props: ImageProfileProps) => {
           style={{ borderRadius: '75px', overflow: 'hidden' }}
         />
       </ImageAvatarWrapper>
+      <CameraIconCoverWrapper>
+          <Camera/>
+      </CameraIconCoverWrapper>
+      <CameraIconAvatarWrapper>
+      <Camera/>
+          {/* <Icon name={'camera'} color={'white'} size={'40px'}/> */}
+      </CameraIconAvatarWrapper>
     </ImageProfileWrapper>
   )
 })
