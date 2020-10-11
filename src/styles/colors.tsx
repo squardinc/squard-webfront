2f1f8869d8d5d9815c19f4d5230741af1b18d0c4
nbr41to <nbr.41to@gmail.com>
2020-10-11 16:49:41 +0900

    カラーを統一&オブジェクト化
    オブジェクト化に意見があればお願いします

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -1,6 +1,27 @@
+// 各時import型
 export const textBlack = '#262626'
 export const textWhite = '#fff'
 export const bgBlack = '#262626'
 export const gradientYellow = '#fbe62e'
 export const gradientRed = '#af1a3d'
 export const borderGray = '#999b9c'
+// variables
+export const bgBrack = ' #051026'
+export const brack = '#051026'
+export const yellow = '#fbe62e'
+
+// オブジェクト型
+const colors = {
+    textBlack: '#262626',
+    textWhite: '#fff',
+    bgBlack: '#262626',
+    gradientYellow: '#fbe62e',
+    gradientRed: '#af1a3d',
+    borderGray: '#999b9c',
+    // variables
+    black: '#051026',
+    yellow: '#fbe62e',
+
+}
+
+export default colors
