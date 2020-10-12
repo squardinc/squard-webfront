1fed781b45aa99b5fdd954c3dc51db700bb39121
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-13 06:29:01 +0900

    fix static routing

--- src/contents/SocialSignIn.tsx
+++ src/contents/SocialSignIn.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
-import { AuthModal, ModalType } from 'src/components/Modal/AuthModal'
-import { TopLayout } from './TopLayout'
-import { UserContext } from 'src/context/UserContext'
 import { CompleteModal } from 'src/components/Modal/CompleteModal'
+import { UserContext } from 'src/context/UserContext'
+import { TopLayout } from './TopLayout'
 
 export const SocialSigninLayout: React.FC = () => {
   const { user } = React.useContext(UserContext)
--- src/contents/callback/ResetPassword.tsx
+++ src/contents/callback/ResetPassword.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react'
-import { TeamLayout } from 'src/contents/team/TeamLayout'
-import { parseSearchParams } from 'src/utils/UrlParser'
 import { navigate } from 'gatsby'
+import * as React from 'react'
 import { PasswordResetModal } from 'src/components/Modal/PasswordResetModal'
+import { parseSearchParams } from 'src/utils/UrlParser'
+import { TopLayout } from '../TopLayout'
 
 export const ResetPasswordLayout: React.FC = () => {
   const params = parseSearchParams(window.location.search)
   return (
     <>
-      <TeamLayout />
+      <TopLayout />
       <PasswordResetModal
         username={params.id}
         code={params.code}
