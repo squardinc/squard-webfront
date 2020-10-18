f702fc1dba014aa694151cdd2ba6d48db1746308
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-18 13:07:34 +0900

    add Join Galleries mutation

--- src/contents/team/Join/JoinTeamContainer.tsx
+++ src/contents/team/Join/JoinTeamContainer.tsx
@@ -4,14 +4,16 @@ import JoinTeam from 'src/contents/team/Join/JoinTeamPage'
 import { withTheme } from 'src/context/ThemeContext'
 import { UserContext } from 'src/context/UserContext'
 import { checkout } from 'src/external/stripe'
-import { requestSubscription } from 'src/graphql/mutations'
+import { joinAsGalleries, requestSubscription } from 'src/graphql/mutations'
 import { getTeam } from 'src/graphql/queries'
 import { ClassType } from 'src/models/person'
 import { TeamClass } from 'src/models/team'
 import {
   GetTeamQuery,
+  JoinAsGalleriesMutation,
+  JoinAsGalleriesMutationVariables,
   RequestSubscriptionMutation,
-  RequestSubscriptionMutationVariables,
+  RequestSubscriptionMutationVariables
 } from 'src/types/API'
 import { parseSearchParams } from 'src/utils/UrlParser'
 
@@ -27,6 +29,10 @@ const JoinTeamContainer: React.FC<JoinTeamContainerProps> = ({ teamId }) => {
     RequestSubscriptionMutation,
     RequestSubscriptionMutationVariables
   >(gql(requestSubscription))
+  const [requestJoinAsGalleries, joinAsGalleriesResponse] = useMutation<
+    JoinAsGalleriesMutation,
+    JoinAsGalleriesMutationVariables
+  >(gql(joinAsGalleries))
   const { loading, error, data } = useQuery<GetTeamQuery>(gql(getTeam), {
     variables: { id: teamId },
   })
@@ -44,9 +50,7 @@ const JoinTeamContainer: React.FC<JoinTeamContainerProps> = ({ teamId }) => {
   const team = data.getTeam
   const teamClasses = team.classes || []
   const joinableTeamClasses = JoinableClasses.map((joinableClass) => {
-    const teamClass = teamClasses.find(
-      (each) => each?.classType === joinableClass
-    )
+    const teamClass = teamClasses.find((each) => each?.classType === joinableClass)
     return new TeamClass(
       teamClass?.teamId,
       teamClass?.teamClassId,
