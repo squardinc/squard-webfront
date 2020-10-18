205913278d7042d18fd2612a1e748b7fe608a219
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-19 00:14:58 +0900

    update sctl

--- src/contents/sctl/SctlPage.tsx
+++ src/contents/sctl/SctlPage.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import { TextDisplay } from 'src/components/TextDisplay/TextDisplay'
-import styles from './sctl.module.scss'
-import { withTheme } from 'src/context/ThemeContext'
 import { DefaultFooter } from 'src/components/Footer/ContentFooter'
+import { TextDisplay } from 'src/components/TextDisplay/TextDisplay'
+import { withTheme } from 'src/context/ThemeContext'
+import styles from './sctl.module.scss'
 
 interface PolicySectionContent {
   section?: string | JSX.Element
@@ -68,9 +68,7 @@ const Page: React.FC = () => {
       </div>
       <PolicyChapter chapter="事業者">スクアード株式会社</PolicyChapter>
       <PolicyChapter chapter="運営責任者">小池駿平</PolicyChapter>
-      <PolicyChapter chapter="所在地">
-        〒135-0064 東京都江東区青海2-7-4 the SOHO 1310
-      </PolicyChapter>
+      <PolicyChapter chapter="所在地">〒135-0064 東京都江東区青海2-7-4 the SOHO 1310</PolicyChapter>
       <PolicyChapter chapter="電話番号">
         03-5962-4062
         <br />
@@ -81,44 +79,50 @@ const Page: React.FC = () => {
           お問い合わせ
         </a>
         <br />
-        チームオーナーへのお問い合わせは、各チームによって定められた方法によって行ってください。
+        各チームへのお問い合わせは、各チームによって定められた方法によって行ってください。
       </PolicyChapter>
       <PolicyChapter chapter="役務の対価">
         <PolicySectionWithSubSections
           sections={[
             {
-              subsections: [
-                'チームオーナーの支払う対価',
-                '各チームに支援するPROSPECTS、ANGELS、V.I.Pなど、それぞれのメンバーから受けた支援金に下記の手数料を掛けたものが当社の手数料となります。',
-                'PROSPECTS: 手数料 10%',
-                'ANGELS: 手数料 10%',
-                'V.I.P: 手数料 10%',
-              ],
+              section: 'チームマスターの支払う対価',
             },
             {
               section:
-                '金額は各チームごとに異なるため、支援の際には各チームページ上に記載された内容をご確認ください。なお、支援金の支払いは、リターンの対価としてチームオーナーへ支払われるものであり、当社は代理受領者として支援金を授受するにすぎません。詳しくは利用規約をご参照ください。',
+                '各チームに対してユーザーから支払われるクラス毎の会員費に下記の手数料を掛けたものが当社の手数料となります。',
+              subsections: ['チーム会員費・・・手数料 : 10%（税別）'],
+            },
+            {
+              subsections: [
+                '会員費はチーム毎に異なるため、会員登録の際には各チームページ上に記載された内容をご確認ください。',
+                'なお、会員費の支払いは、会員特典の対価としてチームマスターへ支払われるものであり、当社は代理受領者として会員費を授受するにすぎません。',
+                '詳しくは利用規約をご参照ください。',
+              ],
             },
           ]}
         />
       </PolicyChapter>
       <PolicyChapter chapter="支払方法">
-        <PolicySection
+        <PolicySectionWithSubSections
           sections={[
-            'チームオーナーの支払い方法',
-            '手数料の支払いは、総支援金額から手数料相当額を差し引くことにより行われます。なお、銀行への振込手数料等その他の費用は弊社が負担します。',
+            {
+              section: 'チームマスターの支払い方法',
+              subsections: [
+                '手数料の支払いは、合計会員費から手数料相当額を控除することにより行われます。なお、銀行への振込手数料はチームマスターの負担となります。',
+              ],
+            },
           ]}
         />
       </PolicyChapter>
       <PolicyChapter chapter="支払い時期">
         <PolicySection
           sections={[
-            '支援者から受けた支援金は毎月月末を締め日とし、締め日翌月の16日以降に「支援金明細」を発行し、弊社の手数料相当額を支援金額から差し引いた金額を、締め日翌月の16日以降にお支払いいたします。',
-            '入金時期の例：1月分（1/1~1/31）は2/16以降に入金',
+            'チーム会員から受けた会員費は毎月月末を締め日とし、弊社の手数料相当額を合計会員費から控除した金額を、締め日翌月の16日以降同月末日限りお支払いいたします。また、特定のクレジットカードブランド（JCB/Diners Club/Discover）で決済された会員費のみ、システムの都合上、支払期限を過ぎる場合があることを予め了承します。',
+            '尚、会員費の明細については別途お問い合わせください。',
           ]}
         />
       </PolicyChapter>
-      <PolicyChapter chapter="役務の内容およびその提供時">
+      <PolicyChapter chapter="役務の内容およびその提供時期">
         <PolicySection
           sections={[
             '当社は、会員同士がチームを結成する為のマッチングの場を提供し、結成したチームに対し、他の会員が支援者となり、そのチームへ金銭的な支援をする両者のマッチングの場を提供しています。したがって、当社からチームオーナーへの役務の提供時期は、各チームの結成から各チームの解散までとなります。',
@@ -129,9 +133,10 @@ const Page: React.FC = () => {
       <PolicyChapter chapter="返金・キャンセル">
         <PolicySection
           sections={[
-            '本サービスに結成され、支援金の支払いが可能になったチームは原則、当社の承諾なくチームの解散やメンバー変更、クラスに応じた支援金の設定変更やリターン内容の変更が可能です。各チームの変更内容についてのご質問は、各チームへお問い合わせください。',
-            '定期購読の解除は定期購読解除のページから行ってください。定期購読解除日以降の決済がキャンセルされます。',
-            'なお、一度決済された各チームへの支援金の返金・キャンセルの受付は、一切お断りさせていただきます。',
+            '本サービスに結成されたチームは、やむを得ない理由でチームの継続ができない場合や、プロジェクト達成後など、チームマスターが必要と判断した場合には、チームマスターがチームの定める方法でチーム会員に解散を事前に告知した上で、自己の責任でチームを解散することができます。',
+            'また、チームマスターは、やむを得ない事情により会員特典の内容やクラス毎の会員費の変更等が必要である場合には、自己の責任で事前にチーム会員へ個別に告知することで会員特典の内容やクラス毎の会員費の変更することが出来ます。各チームの変更内容についてのご質問は、各チームへお問い合わせください。',
+            'チームの会員登録の解除はチームの会員登録解除のページから行ってください。チームの会員登録解除日が属する暦月の翌月以降の決済がキャンセルされます。',
+            'なお、一度決済された各チームへの会員費の返金・キャンセルの受付は、一切お断りさせていただきます。',
           ]}
         />
       </PolicyChapter>
@@ -139,20 +144,18 @@ const Page: React.FC = () => {
       <div className="pt-8 text-center text-white text-2xl font-bold tracking-wider ">
         <TextDisplay className="flex justify-center">
           <div className={styles.sharpUnderLiner}>
-            チームにおけるリターンの
+            チームにおける
             <br />
-            販売に関する共通事項
+            会員特典に関する共通事項
           </div>
         </TextDisplay>
       </div>
       <div className="pb-8">
         <TextDisplay className="pt-2 pl-16 pr-16 text-justify text-white text-sm font-thin tracking-widest text-opacity-75">
-          各チームにおけるリターンの販売者はチームオーナーであり、当社ではありません。以下の事項および、各チームページの記載をご確認ください。
+          各チームにおける特典の提供者はチームマスターであり、弊社ではありません。以下の事項および、各チームページの記載をご確認ください。
         </TextDisplay>
       </div>
-      <PolicyChapter chapter="チームの名称">
-        各チームページの記載に準じる。
-      </PolicyChapter>
+      <PolicyChapter chapter="チームの名称">各チームページの記載に準じる。</PolicyChapter>
       <PolicyChapter
         chapter={
           <div>
@@ -164,30 +167,32 @@ const Page: React.FC = () => {
       >
         各チームページの記載に準じる。
       </PolicyChapter>
-      <PolicyChapter chapter="お問い合わせ先">
-        各チームページの記載に準じる。
-      </PolicyChapter>
+      <PolicyChapter chapter="お問い合わせ先">各チームページの記載に準じる。</PolicyChapter>
       <PolicyChapter chapter="メールアドレス（電子メールで広告を行う場合）">
         各チームページの記載に準じる。
       </PolicyChapter>
-      <PolicyChapter chapter="商品の販売価格・役務の対価">
+      <PolicyChapter chapter="クラス毎の会員価格・役務の対価">
         各チームページの記載に準じる。
       </PolicyChapter>
       <PolicyChapter chapter="対価以外に必要となる費用">
         各チームページの記載に準じる。
       </PolicyChapter>
       <PolicyChapter chapter="代金の支払時期">
-        各チームへの支援金はチーム加入時の決済画面で手続きを済ませ、決済完了画面が表示された時点で初回の決済が行われます。
+        各チームへの会員費はチーム加入時の決済画面で手続きを済ませ、決済完了画面が表示された時点で初回の決済が行われます。
         初回決済以降は決済後1ヶ月経過毎に設定した支払い方法で決済されます。
         クレジットカードの決済：決済画面より支払いが完了した時点で決済が行われます。
       </PolicyChapter>
-      <PolicyChapter chapter="購読期間">
-        決済毎の購読期間は決済日を起点に1ヶ月間となり、購読期間終了日に自動更新されます。
+      <PolicyChapter chapter="会員期間">
+        決済毎の会員期間は決済日を起点に1ヶ月間となり、会員期間終了日に自動更新されます。
       </PolicyChapter>
       <PolicyChapter chapter="支払方法">
         クレジットカード払い
         <br />
         (Visa/Mastercard/JCB/Diners Club/American Express)
+      </PolicyChapter>
+
+      <PolicyChapter chapter="商品引渡しまたは役務提供の時期">
+        各チームへの会員費の決済が行われた時点で、約定された特典を得る権利が発生します。なお、特典毎の履行時期は各チームの記載・連絡に準じます。諸般の事情により遅延が生じる場合は、チームマスターより記載・連絡されるものとします。
       </PolicyChapter>
 
       <DefaultFooter />
