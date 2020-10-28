dd9c4251ad20dfd8450c81aee1cd8f69125dd7a7
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-28 13:12:56 +0900

    move errormessage definition to component

--- src/components/Modal/ErrorMessageModal.tsx
+++ src/components/Modal/ErrorMessageModal.tsx
@@ -1,30 +1,48 @@
 import * as React from 'react'
 import { LoadingContext } from 'src/context/LoadingContextProvider'
 import { ErrorType } from 'src/types/ErrorType'
-import { errorMessage } from 'src/utils/errorMessage'
+import { AgreementsModal } from './AgreementsModal'
 import { asModal, ModalProps } from './asModal'
 import { MessageModal } from './MessageModal'
 
+export const errorMessage = (errorType?: ErrorType) => {
+  if (errorType === 'NOT_JOINED') return 'チームに参加していません。'
+  if (errorType === 'ALREADY_JOINED') return '既に参加しています。'
+  if (errorType === 'ALREADY_LEFT') return 'チームに参加していないか、既に脱退しています。'
+  return (
+    <div>
+      サーバーとの通信に失敗しました。再度お試し頂くか、
+      <a href="mailto:contact@squard.co.jp">運営者</a>へお問い合わせ下さい。
+    </div>
+  )
+}
+
 type ErrorMessageComponentProps = ModalProps & {
+  message?: string | JSX.Element
   errorType?: ErrorType
   buttonText?: string
 }
-export const ErrorMessageCompoenent: React.FC<ErrorMessageComponentProps> = ({
+const ErrorMessageComponent: React.FC<ErrorMessageComponentProps> = ({
   closeModal,
+  message,
   errorType,
   buttonText = 'Close',
+  closable,
 }) => {
   const { setLoading } = React.useContext(LoadingContext)
   React.useEffect(() => {
     setLoading(false)
   }, [])
+  if (errorType === 'AREEMENT_TO_TERMS_OF_USE_REQUIRED')
+    return <AgreementsModal closeModal={() => closeModal} closable={closable} />
   return (
     <MessageModal
       closeModal={closeModal}
-      message={errorMessage(errorType)}
+      message={message ? message : errorMessage(errorType)}
       buttonText={buttonText}
+      closable={closable}
     />
   )
 }
 
-export const ErrorMessageModal = asModal(ErrorMessageCompoenent)
+export const ErrorMessageModal = asModal(ErrorMessageComponent, false)
--- src/types/ErrorType.ts
+++ src/types/ErrorType.ts
@@ -1,2 +1,8 @@
-const ERROR_TYPES = ['BAD_REQUEST', 'ALREADY_JOINED', 'NOT_JOINED', 'ALREADY_LEFT'] as const
+const ERROR_TYPES = [
+  'BAD_REQUEST',
+  'ALREADY_JOINED',
+  'NOT_JOINED',
+  'ALREADY_LEFT',
+  'AREEMENT_TO_TERMS_OF_USE_REQUIRED',
+] as const
 export type ErrorType = typeof ERROR_TYPES[number]
