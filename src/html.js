eeaf080ef964349176a0f1aa99652e62fd8ef99e
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-29 21:17:15 +0900

    add waning on javascript disabled

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <noscript>本サービスを利用するにはJavascriptを有効にする必要があります</noscript>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
