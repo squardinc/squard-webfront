4eedeb7d64782c8d628b3f8fecb473e2d3028f4e
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-29 23:18:33 +0900

    change start url

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
       options: {
         name: `Squard`,
         short_name: `Squard`,
-        start_url: `/squard`,
+        start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
