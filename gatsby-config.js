66ae9693965911ab1d4be899e4fb9552a0a3d293
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-29 22:47:24 +0900

    diable gatsby-plugin-offline options

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,47 +57,47 @@ module.exports = {
         icon: `src/images/logo.png`,
         icons: [
           {
-            "src": "icon/icon-48x48.png",
-            "sizes": "48x48",
-            "type": "image/png",
-            "purpose": "any maskable"
+            src: 'icon/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            "src": "icon/icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png",
-            "purpose": "any maskable"
+            src: 'icon/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            "src": "icon/icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png",
-            "purpose": "any maskable"
+            src: 'icon/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            "src": "icon/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png",
-            "purpose": "any maskable"
+            src: 'icon/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            "src": "icon/icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any maskable"
+            src: 'icon/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            "src": "icon/icon-256x256.png",
-            "sizes": "256x256",
-            "type": "image/png",
-            "purpose": "any maskable"
+            src: 'icon/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            "src": "icon/icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any maskable"
-          }
+            src: 'icon/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
         ],
         cache_busting_mode: 'none',
       },
@@ -131,14 +131,15 @@ module.exports = {
         verbose: true,
       },
     },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/*`],
-        workboxConfig: {
-          globPatterns: ['**/icon-path*'],
-        },
-      },
-    },
+    `gatsby-plugin-offline`,
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [`/*`],
+    //     workboxConfig: {
+    //       globPatterns: ['**/icon-path*'],
+    //     },
+    //   },
+    // },
   ],
 }
