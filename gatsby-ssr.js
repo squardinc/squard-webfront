0e35d0326698b140b405d1f57faa8ad2e49d9600
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-30 15:23:31 +0900

    remove

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,4 +5,4 @@
  */
 
 // You can delete this file if you're not using it
-exports.replaceRenderer = ({ replaceBodyHTMLString }) => { replaceBodyHTMLString('<div id="___gatsby"></div>'); };
+// exports.replaceRenderer = ({ replaceBodyHTMLString }) => { replaceBodyHTMLString('<div id="___gatsby"></div>'); };
