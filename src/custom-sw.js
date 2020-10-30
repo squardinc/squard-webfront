3de17a767f554281af48ea1653e3c869a854c59c
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-10-30 15:41:17 +0900

    remove unused

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -1,8 +1,3 @@
-// show a notification after 15 seconds (the notification
-// permission must be granted first)
-setTimeout(() => {
-self.registration.showNotification("Hello, world!")
-}, 15000)
 const customRoute = new workbox.routing.NavigationRoute((props) => {
     console.log('routing!')
     console.log(props)
